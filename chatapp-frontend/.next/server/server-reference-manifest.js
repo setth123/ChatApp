bc88c715b895e5bfb4b4b4ad531c3e86e62a1138
Edward Laurent <thanhtkcb2004@gmail.com>
2025-10-09 15:15:47 +0700

    Delete chatapp-frontend/.next directory

--- chatapp-frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yZK14DsnYeKIpmsAoOOxX1sQE6FjnDG0Gk94XqS5lLw=\"\n}"
--- chatapp-frontend/.next/static/chunks/app_page_tsx_ea129683._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_54007c7f._.js",
-  "static/chunks/_5f7d7aaf._.js"
-],
-    source: "dynamic"
-});
